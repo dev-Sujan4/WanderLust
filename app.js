fcf4a78b1fa5c969d8c48b056a3ace3a83d485f9
dev-sujan4 <mail.sujansingh@gmail.com>
2026-08-22 21:21:33 +0530

    modified app.js

--- app.js
+++ app.js
@@ -75,9 +75,9 @@ const sessionOptions = {
 
 
 
-// app.get("/", (req, res) => {
-//   res.send("working");
-// });
+app.get("/", (req, res) => {
+  res.send("working");
+});
 
 
 app.use(session(sessionOptions));
